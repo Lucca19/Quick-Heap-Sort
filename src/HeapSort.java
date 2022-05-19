dbba4b085eae0be455f6203b7a0034902c50a0af
Lucca <62717844+Lucca19@users.noreply.github.com>
2022-05-19 12:17:25 -0300

    Primeiro commit

---
+++ src/HeapSort.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] vetor = new int[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) Math.floor(Math.random() * vetor.length);
+        }
+
+        System.out.println("Vetor sem ordenar:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+
+        
+    }
+}
